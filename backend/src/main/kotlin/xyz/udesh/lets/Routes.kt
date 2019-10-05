6deffdf2b19f17f8c298b108f364acc1b24ec983
Udesh Kumarasinghe <mail@udesh.xyz>
2019-10-06 00:09:39 +0530

    Add cors config to backend

--- backend/src/main/kotlin/xyz/udesh/lets/Routes.kt
+++ backend/src/main/kotlin/xyz/udesh/lets/Routes.kt
@@ -1,17 +1,21 @@
 package xyz.udesh.lets
 
 import org.springframework.context.support.beans
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.CorsWebFilter
 import org.springframework.web.reactive.function.server.router
 import xyz.udesh.lets.handlers.IdeaHandler
 
 val routerBean = beans {
+    bean {
+        CorsWebFilter { CorsConfiguration().applyPermitDefaultValues() }
+    }
     bean {
         router {
             "/ideas".nest {
                 val handler = IdeaHandler(ref())
 
                 GET("/stream") { handler.stream() }
-                GET("/{id}") { handler.get(it) }
                 GET("/") { handler.getAll(it) }
                 POST("/") { handler.create(it) }
             }
