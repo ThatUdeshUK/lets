40bf6e82fe86c8a041ba80e4663431413f4d3205
Udesh Kumarasinghe <mail@udesh.xyz>
2019-10-05 09:33:04 +0530

    Add initial backend

---
+++ backend/src/main/kotlin/xyz/udesh/lets/LetsApplication.kt
@@ -0,0 +1,11 @@
+package xyz.udesh.lets
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LetsApplication
+
+fun main(args: Array<String>) {
+	runApplication<LetsApplication>(*args)
+}
---
+++ backend/src/test/kotlin/xyz/udesh/lets/LetsApplicationTests.kt
@@ -0,0 +1,13 @@
+package xyz.udesh.lets
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LetsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
